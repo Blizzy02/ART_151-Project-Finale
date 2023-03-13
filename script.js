abfb3779207509d7f64d40913b79ad3f52dcd1a3
Blizzy02 <123606053+Blizzy02@users.noreply.github.com>
2023-03-13 03:40:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-const WHITE_KEY_FIRST_OCTAVE = ["a","s","d","f","g","h","j","k"];
-const BLACK_KEY_FIRST_OCTAVE = ["w","e","f","t","y","u","i"];
-const WHITE_KEY_SECOND_OCTAVE = ["A","S","D","F","G","H","J","K"];
-const BLACK_KEY_SECOND_OCTAVE = ["W","E","F","T","Y","U","I"];
-const WHITE_KEY_SMALL_OCTAVE = ["Q"]
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
-
-
-const keys = document.querySelectorAll(".key")
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
-    key.addEventListener('click', () => playNote(key))
+  key.addEventListener('click', () => playNote(key))
 })
 
-document.addEventListener('keydown',e => {
-     const key = e.key;
-     const whiteKeyFirstIndex = WHITE_KEY_FIRST_OCTAVE.indexOf(key);
-     const blackKeyFirstIndex = BLACK_KEY_FIRST_OCTAVE.indexOf(key);
-     const whiteKeySecondIndex = WHITE_KEY_SECOND_OCTAVE.indexOf(key);
-     const blackKeySecondIndex = BLACK_KEY_SECOND_OCTAVE.indexOf(key);
+document.addEventListener('keydown', e => {
+  if (e.repeat) return
+  const key = e.key
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
 })
 
 function playNote(key) {
-    const noteAudio = document.getElementById(key.dataset.note)
-    noteAudio.currentTime = 0
-    noteAudio.play();
-    key.classList.add('active');
-    noteAudio.addEventListener("ended", () => {
-        key.classList.remove('active')
-    })
-
-
-
+  const noteAudio = document.getElementById(key.dataset.note)
+  noteAudio.currentTime = 0
+  noteAudio.play()
+  key.classList.add('active')
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+  })
 }
